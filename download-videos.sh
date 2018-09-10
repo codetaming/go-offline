af8c8a6cbc86068fb0763304dd6fb7a882d09fe2
Daniel Vaughan <dan@danielvaughan.com>
2018-09-10 14:28:12 +0100

    Added GopherCon videos

---
+++ download-videos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+brew install youtube-dl
+#GopherCon UK
+#2018
+youtube-dl https://www.youtube.com/watch?v=2mgKDqD5Ga8&list=PLDWZ5uzn69ewsMyuGjVsAnpQIjyud1Cv9
+#2017
+youtube-dl https://www.youtube.com/watch?v=5UG57xQL_RE&list=PLDWZ5uzn69eyM81omhIZLzvRhTOXvpeX9
+#2016
+youtube-dl https://www.youtube.com/watch?v=LpWEDFT3iLU&list=PLDWZ5uzn69eyh791ZTkEA9OaTxVpGY8_g
+#2015
+youtube-dl https://www.youtube.com/watch?v=CdhucJShJU8&list=PLDWZ5uzn69ezRJYeWxYNRMYebvf8DerHd
+#GopherCon
+#2017
+youtube-dl https://www.youtube.com/watch?v=ha8gdZ27wMo&list=PL2ntRZ1ySWBdD9bru6IR-_WXUgJqvrtx9
+#2016
+youtube-dl https://www.youtube.com/watch?v=KINIAgRpkDA&list=PL2ntRZ1ySWBdliXelGAItjzTMxy2WQh0P
+#2015
+youtube-dl https://www.youtube.com/watch?v=3EW1hZ8DVyw&list=PL2ntRZ1ySWBf-_z-gHCOR2N156Nw930Hm
