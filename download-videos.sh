efc2384d1f7fd253cea543fb9648fbdc1014b5b4
Daniel Vaughan <dan@danielvaughan.com>
2018-09-11 20:21:02 +0100

    Added GopherCon videos

--- download-videos.sh
+++ download-videos.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 brew install youtube-dl
+md "GopherCon"
+cd "GopherCon"
+md "2018"
+cd "2018"
+youtube-dl https://www.youtube.com/watch?v=zPd0Cxzsslk&list=PL2ntRZ1ySWBdatAqf-2_125H4sGzaWngM
+md "London Gophers"
+cd "London Gophers"
+md "August 2018"
+cd "August 2018"
+youtube-dl https://www.youtube.com/watch?v=7cEp98y6WCs&list=PL8QGElREVyDA2iDrPNeCe8B1u7li5S6ep
+#June 2018
+youtube-dl https://www.youtube.com/watch?v=0XW6wI2FnPA&list=PL8QGElREVyDB1y7q-tarloT2PHhOP-ZIK
+#May 2018
+youtube-dl https://www.youtube.com/watch?v=eTjL3grAYAM&list=PL8QGElREVyDBLA4OXGvNELXAtSznHK_fw
+#March 2018
+youtube-dl https://www.youtube.com/watch?v=kWAxBhsEayk&list=PL8QGElREVyDDDtiVmCjh_zBkY4KnZmHxb
+#January 2018
+youtube-dl https://www.youtube.com/watch?v=JqnqDeRZxas&list=PL8QGElREVyDDimqKDNDRpB9MPTq_dPIc5
+#November 2017
+youtube-dl https://www.youtube.com/watch?v=BBiIr19JOo4&list=PL8QGElREVyDCMDKnpmkvPzyE2DJRaRISj
+#January 2018
+youtube-dl https://www.youtube.com/watch?v=JqnqDeRZxas&list=PL8QGElREVyDDimqKDNDRpB9MPTq_dPIc5
 #GopherCon UK
 #2018
 youtube-dl https://www.youtube.com/watch?v=2mgKDqD5Ga8&list=PLDWZ5uzn69ewsMyuGjVsAnpQIjyud1Cv9
