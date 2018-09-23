fdd4e3f9cd11b26ebc91429606168dff23d722de
Daniel Vaughan <dan@danielvaughan.com>
2018-09-23 10:02:59 +0100

    added kubernetes videos

---
+++ download-k8s-videos.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+mkdir "k8s"
+cd "k8s"
+mkdir "videos"
+cd "videos"
+mkdir "Kubernetes Best Practices"
+cd "Kubernetes Best Practices"
+youtube-dl https://www.youtube.com/playlist?list=PLIivdWyY5sqL3xfXz5xJvwzFW_tlQB_GB
